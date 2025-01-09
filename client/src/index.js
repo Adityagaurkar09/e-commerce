675897743ed355b3400b10401be2f9ebebfabd31
Adityagaurkar09 <adityagaurkar09@gmail.com>
2025-01-09 10:34:10 +0530

    client initial setup

---
+++ client/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+   <>
+    <h1 class="text-red-500 font-bold underline">
+    Hello world!
+  </h1> </>
+);
+
+                                          
